1a4e1b3b1ed749087e3acaff4a83fb1028229f65
David Salas <dasa.19983@gmail.com>
2024-04-21 14:18:58 -0500

    init

---
+++ src/app/gifs/components/search-box/search-box.components.ts
@@ -0,0 +1,29 @@
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
+
+@Component({
+  selector:'gifs-search-box',
+  template:`
+    <h5>Buscar:</h5>
+    <input type="text"
+    class="form-control"
+    placeholder="Buscar gifs..."
+    (keyup.enter)="searchTag()"
+    #txtTagInput
+    >
+  `
+})
+export class SearchBoxComponent{
+
+  constructor(private gisService:GifsService){}
+
+  @ViewChild('txtTagInput')
+  public tagInput!:ElementRef<HTMLInputElement>;
+
+  //searchTag(newTag:string){
+  searchTag(){
+    const newTag = this.tagInput.nativeElement.value;
+    this.gisService.searchTag(newTag);
+    this.tagInput.nativeElement.value="";
+  }
+}
